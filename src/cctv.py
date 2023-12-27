16377e55deb9df84f27a8521182bd3efe5aa808b
Daniel Hernandez <barneyrubble.70777@gmail.com>
2023-12-27 14:32:52 -0600

    only checking if a persona is detected

--- src/cctv.py
+++ src/cctv.py
@@ -3,9 +3,9 @@ import torch
 from PIL import Image
 import cv2
 from utils.env_handler import EnvHandler
-import requests
+import time
 
-#Load environment configs
+# Load environment configs
 config = EnvHandler()
 
 # Load the pre-trained processor and model
@@ -20,6 +20,7 @@ while cap.isOpened():
     ret, frame = cap.read()
 
     if not ret:
+        print("Video stream ended.")
         break
 
     # Convert the frame to PIL Image
@@ -33,15 +34,25 @@ while cap.isOpened():
     target_sizes = torch.tensor([image.size[::-1]])
     results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
 
-    # Draw bounding boxes on the frame
+    # Draw bounding boxes on the frame for detected persons
     for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
         box = [round(i, 2) for i in box.tolist()]
-        label_str = model.config.id2label[label.item()]
-        confidence_str = round(score.item(), 3)
 
-        cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 2)
-        cv2.putText(frame, f"{label_str} {confidence_str}", (int(box[0]), int(box[1]) - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        # Check if the detected object is a person (label ID 1)
+        if label.item() == 1:
+            label_str = model.config.id2label[label.item()]
+            confidence_str = round(score.item(), 3)
+
+            # Get the current system time
+            current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+
+            # Print the system time when a person is detected
+            print(f"Person detected at {current_time}, Confidence: {confidence_str}")
+
+            # Draw bounding box and label on the frame
+            cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 2)
+            cv2.putText(frame, f"{label_str} {confidence_str}", (int(box[0]), int(box[1]) - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     # Display the resulting frame
     cv2.imshow('Object Detection', frame)
